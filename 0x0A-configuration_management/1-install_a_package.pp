84345ab72ee3f6c14225172f2f97e6d844760a47
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-01 12:39:40 +0300

    correction

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,31 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'python3-setuptools':
+  ensure => installed,
+}
+
+package { 'build-essential':
+  ensure => installed,
+}
+
+package { 'python3-dev':
+  ensure => installed,
+}
+
+class { 'python3': }
+
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => [
+    Package['python3-pip'],
+    Package['python3-setuptools'],
+    Package['build-essential'],
+    Package['python3-dev'],
+    Class['python3'],
+  ],
 }
