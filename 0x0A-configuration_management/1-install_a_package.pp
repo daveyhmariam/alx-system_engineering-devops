a0de51638c0891fe1f7a7e8b03991c0e410414b5
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-03 00:35:46 +0300

    install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,31 +1,6 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'python3-setuptools':
-  ensure => installed,
-}
-
-package { 'build-essential':
-  ensure => installed,
-}
-
-package { 'python3-dev':
-  ensure => installed,
-}
-
-class { 'python3': }
-
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => [
-    Package['python3-pip'],
-    Package['python3-setuptools'],
-    Package['build-essential'],
-    Package['python3-dev'],
-    Class['python3'],
-  ],
+  provider => 'pip3'
 }
