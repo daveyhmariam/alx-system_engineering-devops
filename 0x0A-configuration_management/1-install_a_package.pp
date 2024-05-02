b913a1a6d3e7fa8edbaab8d6cdcecb124bf664ef
Dawit Yilma Habtemariam <daveyhmariam@gmail.com>
2024-05-03 00:41:23 +0300

    install flask using puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,9 @@
-# Define a class to manage the installation of Flask
-class install_flask {
-  
-  # Define the command to install Flask version 2.1.0 using pip3
-  $pip_command = '/usr/bin/pip3'
-  $flask_version = '2.1.0'
-  
-  exec { 'install_flask':
-    command     => "${pip_command} install Flask==${flask_version}",
-    path        => ['/usr/bin', '/bin'],
-    unless      => "${pip_command} show Flask | grep Version | grep ${flask_version}",
+class { 'flask':
+  # Ensure Flask is installed with pip3
+  package { 'flask':
+    ensure => present,
+    provider => 'pip3',
+    require => Class['python3'], 
   }
+  pip_version => '2.1.0',
 }
